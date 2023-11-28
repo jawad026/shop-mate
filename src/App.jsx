73117023b22055b297fdef764605f3b82f4e370b
hashim-tayyab <hashim.tayyab.shah@vaival.com>
2023-11-28 10:28:04 +0500

    text box added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Sidebar from "./components/Sidebar/Sidebar";
+import InputBox from "./components/common/InputBox/InputBox";
 
 function App() {
 
   return (
     <>
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <div style={{height: '50px'}}>
+      {/* <InputBox height='20px' width='200px' label='Type Something'/> */}
+      </div>
     </>
   );
 }
