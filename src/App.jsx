8764289152e9350363c6cf6f82376102ee078de0
alisher1129 <ali.sher@vaival.com>
2023-11-29 15:04:38 +0500

    radio

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 
+import AutoProductSelection from "./components/Auto Product Selection/AutoProductSelection";
 import CheckBox from "./components/Buttons/CheckBox";
 import CheckOutButton from "./components/Buttons/CheckOutButton";
+import RadioButton from "./components/Buttons/RadioButton";
 import SimpleButton from "./components/Buttons/SimpleButton";
 import ToggleButton from "./components/Buttons/ToggleButton";
+import Product from "./components/Product/Product";
 import Sidebar from "./components/Sidebar/Sidebar";
 
-
-import "./App.css";
 function App() {
   return (
     <>
 
       {/* <Sidebar /> */}
 
-      <CheckOutButton />
+      {/* <CheckOutButton />
       <SimpleButton />
       <ToggleButton />
-      <CheckBox />
-
-    <div>Run Smooth</div>
-
+      <CheckBox /> */}
+{/* <Product/> */}
+  {/* <AutoProductSelection/> */}
+{/* <RadioButton/> */}
     </>
   );
 }
---
+++ src/components/Buttons/RadioButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function RadioButton() {
+  return (
+    <>
+      
+      <div class="flex items-center">
+        <input
+          checked
+          id="default-radio-2"
+          type="radio"
+          value=""
+          name="default-radio"
+          class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+        />
+        
+      </div>
+    </>
+  );
+}
+
+export default RadioButton;
