61b2b9c3b30cf70ed87a61a3c6213009de8d171c
jawad026 <muhammad.jawad.hassan@vaival.com>
2023-11-30 10:57:49 +0500

    setting

--- src/components/Buttons/CheckOutButton.jsx
+++ src/components/Buttons/CheckOutButton.jsx
@@ -1,8 +1,9 @@
-function CheckOutButton(props) {
+// eslint-disable-next-line react/prop-types
+function CheckOutButton({ text }) {
   return (
     <div>
-      <button className=" w-28 h-10 bg-blue-500 hover:bg-white text-white font-semibold hover:text-blue-500 py-2 px-4 border border-blue-500 hover:border-blue-500 rounded">
-        {props.text}
+      <button className="  h-10 bg-blue-500 hover:bg-white text-white font-semibold hover:text-blue-500 py-2 px-4 border border-blue-500 hover:border-blue-500 rounded">
+        {text}
       </button>
     </div>
   );
