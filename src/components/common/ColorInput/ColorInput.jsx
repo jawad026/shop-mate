3de8b2ec6a5504900e32aa2f4059e77f61894733
jawad026 <muhammad.jawad.hassan@vaival.com>
2023-12-06 16:05:57 +0500

    intallation page

--- src/components/common/ColorInput/ColorInput.jsx
+++ src/components/common/ColorInput/ColorInput.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable react/prop-types */
 
 const InputBox = ({ disabled, required, onchange, name, value, type }) => {
@@ -23,29 +22,3 @@ const InputBox = ({ disabled, required, onchange, name, value, type }) => {
 };
 
 export default InputBox;
-=======
-import React from "react";
-
-const InputBox = ({label, disabled, required, onchange, name, value, type, color}) => {
-  return (     
-      <input
-        type={type || "text"}
-        name={name}
-        value={value}
-        // className="border-[2px]  pb-2.5 pt-4 w-full text-sm 
-        // text-gray-900 bg-transparent rounded-lg 
-        // focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        id="floating_outlined"
-        className="border-[2px]  pb-2.5 pt-4 w-full text-sm 
-        text-gray-900 bg-transparent rounded-lg 
-        focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        placeholder=" "
-        onChange={onchange}
-        disabled = {disabled ? disabled : false}
-        required = {required ? required : true}
-      />      
-  );
-};
-
-export default InputBox;
->>>>>>> d1c27e7b2dedc7b8ab130bd88d29adfd99dbc2b9
